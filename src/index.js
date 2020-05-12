752cba1c9431287e1c43170aa026311fadaa5fd1
nitin-codr <nitinporwal99@gmail.com>
2020-05-12 17:33:51 +0530

    action with middleware setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
+const state=createStore(reducers, applyMiddleware(thunk));
+
 ReactDOM.render(
-    <Provider store={createStore(reducers)} >
+    <Provider store={state} >
         <App />
     </Provider>,
     document.querySelector('#root')
