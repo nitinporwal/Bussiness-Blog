7475a6592b67ff6f9df5bc03a588da6a51ab3810
nitin-codr <nitinporwal99@gmail.com>
2020-05-12 16:06:12 +0530

    PostList component added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PostList from './PostList';
 
 const App = () => {
     return (
         <div>
-            App
+            <PostList />
         </div>
     );
 };
---
+++ src/components/PostList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PostList extends Component {
+    render() {
+        return (
+            <div>
+                PostList
+            </div>
+        );
+    };
+};
+
+
+export default PostList;
